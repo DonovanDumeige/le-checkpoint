d6006a5713ad29edd2d4b33d12477f98d2dc1987
Donovan Dumeige <donovan.dumeige@gmail.com>
2023-01-04 18:16:14 +0100

    feat: add user logic

---
+++ back/src/user/models/updateUser.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class UpdateUserDTO {
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  username: string;
+}
