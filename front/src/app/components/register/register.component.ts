5a2b60701a54a92b02541ae0a4d38461b7c37168
Donovan Dumeige <donovan.dumeige@gmail.com>
2023-01-12 16:51:11 +0100

    feat: add verification for password

--- front/src/app/components/register/register.component.ts
+++ front/src/app/components/register/register.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { AuthentificationService } from 'src/app/services/authentification.service';
 
+
+class CustomValidators{
+  static passwordContainsNumber (control: AbstractControl): ValidationErrors | null {
+    const regex = /\d/;
+
+    if(regex.test(control.value) && control.value !== null) {
+      return null;
+    } else {
+      return { passwordInvalid: true};
+    }
+  }
+
+  static passwordsMatch(control: AbstractControl): ValidationErrors | null {
+    const password= control.get('password')?.value;
+    const passwordConfirm = control.get('passwordConfirm')?.value;
+
+    if((password === passwordConfirm) && (password !== null && passwordConfirm !== null)){
+      return null;
+    }
+    else {
+      return { passwordNotMatching: true};
+    }
+  }
+}
 @Component({
-
-
-
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
@@ -31,11 +52,12 @@ export class RegisterComponent implements OnInit {
         password: ['', [
           Validators.required,
           Validators.minLength(3),
+          CustomValidators.passwordContainsNumber
         ]],
         passwordConfirm: ['', [Validators.required]]
       },
       {
-        // validators: CustomValidators.passwordMatches
+        validators: CustomValidators.passwordsMatch
       })
     }
 
