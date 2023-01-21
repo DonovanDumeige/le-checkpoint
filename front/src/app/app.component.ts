b77034ac6ac9531b64d10a9be76d791499913946
Donovan Dumeige <donovan.dumeige@gmail.com>
2023-01-21 14:59:52 +0100

    gitflow-feature-stash: angular-create-article-markdown

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from './models/user.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'front';
-
+  userRole = 'admin'
+  user!: User;
   entries = [{
     name: 'Login',
     link: 'login',
@@ -23,7 +25,9 @@ export class AppComponent {
   }
 ]
   constructor(private router: Router) {}
-
+ngOnInit(): void {
+  console.log('Role ?', this.user.role)
+}
   navigateTo(value:any) {
     this.router.navigate(['../', value]);
   }
