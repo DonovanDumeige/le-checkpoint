a73a3bf68877a306b4216739e7e44383f9c910c0
Donovan Dumeige <donovan.dumeige@gmail.com>
2023-01-26 19:30:44 +0100

    chore:change port for database

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -17,7 +17,7 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3310,
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
